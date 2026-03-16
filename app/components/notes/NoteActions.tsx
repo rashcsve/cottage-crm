1fcefd55a3bb181ba51be867392951e269702220
Svetlana <svetlana.rashchupkina@gmail.com>
2026-03-16 19:56:12 +0100

    refactor: use actionsbutton

--- app/components/notes/NoteActions.tsx
+++ app/components/notes/NoteActions.tsx
@@ -1,20 +1,16 @@
 "use client";
 
 import { deleteNoteAction } from "@/components/notes/actions";
+import { ActionButton } from "@/components/ui/ActionsButton";
 
-type NoteActionsProps = {
+interface NoteActionsProps {
   noteId: number;
-};
+}
 
 export function NoteActions({ noteId }: NoteActionsProps) {
   return (
     <form action={deleteNoteAction.bind(null, noteId)}>
-      <button
-        type="submit"
-        className="text-sm font-medium text-red-700 transition hover:text-red-800"
-      >
-        Smazat
-      </button>
+      <ActionButton variant="danger">Smazat</ActionButton>
     </form>
   );
 }
--- app/components/tasks/TaskActions.tsx
+++ app/components/tasks/TaskActions.tsx
@@ -2,11 +2,12 @@
 
 import { deleteTaskAction, toggleTaskAction } from "@/components/tasks/actions";
 import { TaskStatus } from "@/components/tasks/types";
+import { ActionButton } from "@/components/ui/ActionsButton";
 
-type TaskActionsProps = {
+interface TaskActionsProps {
   taskId: number;
   currentStatus: TaskStatus;
-};
+}
 
 export function TaskActions({ taskId, currentStatus }: TaskActionsProps) {
   const handleToggleTaskAction = toggleTaskAction.bind(
@@ -14,28 +15,21 @@ export function TaskActions({ taskId, currentStatus }: TaskActionsProps) {
     taskId,
     currentStatus
   );
+
   const handleDeleteTaskAction = deleteTaskAction.bind(null, taskId);
 
   return (
     <div className="flex flex-wrap gap-2">
       <form action={handleToggleTaskAction}>
-        <button
-          type="submit"
-          className="rounded-xl border border-stone-300 px-4 py-2 text-sm font-medium transition hover:bg-stone-100 cursor-pointer"
-        >
+        <ActionButton>
           {currentStatus === "pending"
             ? "Označit jako hotovo"
             : "Vrátit na čeká"}
-        </button>
+        </ActionButton>
       </form>
 
       <form action={handleDeleteTaskAction}>
-        <button
-          type="submit"
-          className="rounded-xl border border-red-200 px-4 py-2 text-sm text-red-700 font-medium transition hover:bg-red-50 cursor-pointer"
-        >
-          Smazat
-        </button>
+        <ActionButton variant="danger">Smazat</ActionButton>
       </form>
     </div>
   );
