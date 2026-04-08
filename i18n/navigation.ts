aea44014e5131fd54bf4f0e398af197ecf47fb29
Svetlana <svetlana.rashchupkina@gmail.com>
2026-04-08 13:57:37 +0200

    feat: add revalidateLocalizedPath helper

--- i18n/navigation.ts
+++ i18n/navigation.ts
@@ -1,5 +1,10 @@
 import { createNavigation } from "next-intl/navigation";
+import { revalidatePath as nextRevalidatePath } from "next/cache";
 import { routing } from "./routing";
 
 export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing);
+
+export function revalidateLocalizedPath(locale: string, path: string) {
+  nextRevalidatePath(`/${locale}${path}`);
+}
